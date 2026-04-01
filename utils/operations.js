c3c26eb3ffd47e514056c69bfdad92e81230865d
Rawan20000 <rawan_rz@icloud.com>
2026-04-01 13:13:32 +0300

    TODO 4

--- utils/operations.js
+++ utils/operations.js
@@ -1,14 +1,14 @@
 export function add(numbers) {
-    return {}
-    }
+    return numbers.reduce((sum, num) => sum + num, 0);
+}
 export function subtract(numbers) {
-      return {}
+      return numbers.slice(1).reduce((result, num) => result - num, numbers[0]);
     }
 
 export function multiply(numbers) {
-      return {}
+      return numbers.reduce((product, num) => product * num, 1);
     }
 
 export function divide(numbers) {
-      return {}
+      return numbers.slice(1).reduce((result, num) => result / num, numbers[0]);
     }
