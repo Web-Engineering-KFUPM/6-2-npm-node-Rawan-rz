c05d8db1cddad666e2aef8a2bcc9a18fe403e1b9
Rawan20000 <rawan_rz@icloud.com>
2026-04-01 13:37:44 +0300

    TODO 5

--- calculator.js
+++ calculator.js
@@ -6,7 +6,7 @@ const operation = process.argv[2];
 const numbers = process.argv.slice(3);
 if (!isValidOperation(operation)) {
     console.log("Invalid operation. Use: add, subtract, multiply, or divide");
-    return;
+    process.exit(1);
   }
   const nums = parseNumbers(numbers);
   let result;
